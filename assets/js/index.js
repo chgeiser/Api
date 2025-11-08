87227a91244ab42cba2221904c3e03d18c265a53
Christopher Geiser <christopher.geiser@gmail.com>
2025-11-07 22:41:21 -0300

    actual

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,18 @@
-async function getRandomUser() {
+let urlBase = "https://findic.cl/"
+const valorIngresado = document.querySelector("#valorIngresado");
+const tipoMonedas = document.querySelector("#tipoMonedas");
+const resultado = document.querySelector("#resultado");
+const btnBuscar = document.querySelector('#btnBuscar');
+const ctx = document.getElementById('myChart');
+let dataMonedas = {};
+let chart;
+
+async function getValores() {
   try {
     //const res = await fetch("https://mindicador.cl/api");
-    const res = await fetch ("https://findic.cl/api/");
+    const res = await fetch (urlBase + 'api/');
     const data = await res.json();
-    //console.log(data);
-
-    const valorIngresado = document.querySelector("#valorIngresado");
-    const tipoMonedas = document.querySelector("#tipoMonedas");
-    const resultado = document.querySelector("#resultado");
+    dataMonedas = data;
 
     for (let key in data) {
         if (data[key].codigo && data[key].nombre) {
@@ -20,4 +25,64 @@ async function getRandomUser() {
   }
 }
 
-getRandomUser();
+
+
+async function calcularValor() {
+  const monto = parseFloat(valorIngresado.value);
+  const codigo = tipoMonedas.value;
+
+  if (!monto || !codigo) {
+    resultado.innerHTML = "Por favor ingrese un valor y seleccione una moneda.";
+    return;
+  }
+
+  const res = await fetch(`${urlBase}api/${codigo}`);
+  const data = await res.json();
+
+  const valorActual = data.serie[0].valor;
+  const conversion = monto / valorActual;
+
+ resultado.innerHTML = `
+    ${monto} pesos chilenos equivalen a ${conversion.toFixed(2)} ${data.unidad_medida}.
+    <br>Valor actual del ${data.nombre}: ${valorActual} ${data.unidad_medida}.
+  `;
+
+  const ultimos = data.serie.slice(0, 10).reverse();
+  const labels = ultimos.map((label) => new Date(label.fecha).toLocaleDateString());
+  const valores = ultimos.map((valor) => valor.valor);
+
+  renderChart(labels, valores, data.nombre);
+}
+
+btnBuscar.addEventListener("click", calcularValor);
+
+function renderChart(labels, valores, nombre) {
+  if (chart) {
+    chart.destroy(); // elimina gráfico anterior
+  }
+
+  chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: `Historial últimos días (${nombre})`,
+          data: valores,
+          borderWidth: 2,
+          borderColor: "blue",
+          fill: false,
+          tension: 0.2,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: { beginAtZero: false },
+      },
+    },
+  });
+}
+
+
+getValores();
